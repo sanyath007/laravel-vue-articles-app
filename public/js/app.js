55c312647a730ede972fad6a7763b628fb5bcb1b
sanyath007 <sanyath007@gmail.com>
2020-01-25 06:13:04 +0700

    Completed with adding delete, add and edit function.

--- public/js/app.js
+++ public/js/app.js
@@ -44690,6 +44690,77 @@ var render = function() {
     [
       _c("h1", [_vm._v("Articles")]),
       _vm._v(" "),
+      _c(
+        "form",
+        {
+          staticClass: "mb-3",
+          on: {
+            submit: function($event) {
+              $event.preventDefault()
+              return _vm.addArticle()
+            }
+          }
+        },
+        [
+          _c("div", { staticClass: "form-group" }, [
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.article.title,
+                  expression: "article.title"
+                }
+              ],
+              staticClass: "form-control",
+              attrs: { type: "text", placeholder: "Title" },
+              domProps: { value: _vm.article.title },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.article, "title", $event.target.value)
+                }
+              }
+            })
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("textarea", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.article.body,
+                  expression: "article.body"
+                }
+              ],
+              staticClass: "form-control",
+              attrs: { placeholder: "Body" },
+              domProps: { value: _vm.article.body },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.article, "body", $event.target.value)
+                }
+              }
+            })
+          ]),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              staticClass: "btn btn-light btn-block",
+              attrs: { type: "submit" }
+            },
+            [_vm._v("Save")]
+          )
+        ]
+      ),
+      _vm._v(" "),
       _c("nav", { attrs: { "aria-label": "Page navigation example" } }, [
         _c("ul", { staticClass: "pagination" }, [
           _c(
@@ -44760,7 +44831,35 @@ var render = function() {
         return _c("div", { key: article.id, staticClass: "card card-body" }, [
           _c("h3", [_vm._v(_vm._s(article.title))]),
           _vm._v(" "),
-          _c("p", [_vm._v(_vm._s(article.body))])
+          _c("p", [_vm._v(_vm._s(article.body))]),
+          _vm._v(" "),
+          _c("hr"),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              staticClass: "btn btn-warning mb-2",
+              on: {
+                click: function($event) {
+                  return _vm.editArticle(article)
+                }
+              }
+            },
+            [_vm._v("Edit")]
+          ),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              staticClass: "btn btn-danger",
+              on: {
+                click: function($event) {
+                  return _vm.deleteArticle(article.id)
+                }
+              }
+            },
+            [_vm._v("Delete")]
+          )
         ])
       })
     ],
@@ -44914,6 +45013,20 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
@@ -44959,6 +45072,68 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       };
 
       this.pagination = pagination;
+    },
+    deleteArticle: function deleteArticle(id) {
+      var _this2 = this;
+
+      if (confirm('Are you sure?')) {
+        fetch('api/article/' + id, {
+          method: 'delete'
+        }).then(function (res) {
+          return res.json();
+        }).then(function (data) {
+          alert('Article Removed !!');
+          _this2.fetchArticles();
+        }).catch(function (err) {
+          return console.log(err);
+        });
+      }
+    },
+    addArticle: function addArticle() {
+      var _this3 = this;
+
+      if (this.edit === false) {
+        // Add
+        fetch('api/article', {
+          method: 'post',
+          body: JSON.stringify(this.article),
+          headers: {
+            'content-type': 'application/json'
+          }
+        }).then(function (res) {
+          return res.json();
+        }).then(function (data) {
+          alert('Article Added !!');
+          _this3.fetchArticles();
+        }).catch(function (err) {
+          return console.log(err);
+        });
+      } else {
+        // Update
+        fetch('api/article', {
+          method: 'put',
+          body: JSON.stringify(this.article),
+          headers: {
+            'content-type': 'application/json'
+          }
+        }).then(function (res) {
+          return res.json();
+        }).then(function (data) {
+          _this3.article.title = '';
+          _this3.article.body = '';
+          alert('Article Updated !!');
+          _this3.fetchArticles();
+        }).catch(function (err) {
+          return console.log(err);
+        });
+      }
+    },
+    editArticle: function editArticle(article) {
+      this.edit = true;
+      this.article.id = article.id;
+      this.article.article_id = article.id;
+      this.article.title = article.title;
+      this.article.body = article.body;
     }
   }
 });
--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -2,6 +2,16 @@
   <div>
     <h1>Articles</h1>
 
+    <form class="mb-3" @submit.prevent="addArticle()">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Title" v-model="article.title">
+      </div>
+      <div class="form-group">
+        <textarea class="form-control" placeholder="Body" v-model="article.body"></textarea>
+      </div>
+      <button type="submit" class="btn btn-light btn-block">Save</button>
+    </form>
+    
     <nav aria-label="Page navigation example">
       <ul class="pagination">
         <li class="page-item" :class="[{disabled: !pagination.prev_page_url}]">
@@ -25,6 +35,10 @@
     <div class="card card-body" v-for="article in articles" :key="article.id">
       <h3>{{ article.title }}</h3>
       <p>{{ article.body }}</p>
+      <hr />
+
+      <button class="btn btn-warning mb-2" @click="editArticle(article)">Edit</button>
+      <button class="btn btn-danger" @click="deleteArticle(article.id)">Delete</button>
     </div>
   </div>
 </template>
@@ -70,6 +84,61 @@ export default {
       };
 
       this.pagination = pagination;
+    },
+    deleteArticle(id) {
+      if(confirm('Are you sure?')) {
+        fetch(`api/article/${id}`, {
+          method: 'delete'
+        })
+          .then(res => res.json())
+          .then(data => {
+            alert('Article Removed !!');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+      }
+    },
+    addArticle() {
+      if(this.edit === false) {
+        // Add
+        fetch(`api/article`, {
+          method: 'post',
+          body: JSON.stringify(this.article),
+          headers: {
+            'content-type': 'application/json'
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            alert('Article Added !!');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+      } else {
+        // Update
+        fetch(`api/article`, {
+          method: 'put',
+          body: JSON.stringify(this.article),
+          headers: {
+            'content-type': 'application/json'
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            this.article.title = '';
+            this.article.body = '';
+            alert('Article Updated !!');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+      }
+    },
+    editArticle(article) {
+      this.edit = true;
+      this.article.id = article.id;
+      this.article.article_id = article.id;
+      this.article.title = article.title;
+      this.article.body = article.body;
     }
   },
 }
